d91e3022ccaa0661894cbfd10df9737295b60ae4
BFvisuals <brunofonsecavisuals@gmail.com>
2025-07-19 22:08:56 +0100

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-// servidor backend
+// backend/server.js
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("API do Baixa Tudo está online! 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor a correr na porta ${PORT}`);
+});
